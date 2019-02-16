b9daf6e54a02f9d95210bf6a6b1ac4bf54c9bdde
chamalwr <chamalkanishka4@gmail.com>
2019-02-17 01:24:57 +0530

    refactor api endpoints

--- app/src/main/java/com/chamalwr/mtopicnewsreader/MainActivity.java
+++ app/src/main/java/com/chamalwr/mtopicnewsreader/MainActivity.java
@@ -1,16 +1,21 @@
 package com.chamalwr.mtopicnewsreader;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.chamalwr.mtopicnewsreader.functions.ApiClient;
+import com.chamalwr.mtopicnewsreader.functions.JsonPlaceHolder;
+import com.chamalwr.mtopicnewsreader.models.News;
+import com.chamalwr.mtopicnewsreader.models.NewsResponse;
+import com.google.gson.Gson;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +23,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.ViewHolder viewHolder;
     private LinearLayoutManager linearLayoutManager;
-    private Map<String, String> newsArticals;
+    private List<News> list;
+    private Map<String, String> newsList;
+    private ApiClient apiClient = new ApiClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,11 +66,39 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .build();
 
-        ApiClient apiClient = new ApiClient();
         OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
-        Retrofit retrofit = apiClient.getClient(okHttpClient);
-        newsArticals = apiClient.getNewsList();
+        Retrofit retrofit = getClient(okHttpClient);
+    }
 
+
+    public Retrofit getClient(OkHttpClient.Builder httpClient) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://newsapi.org/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        JsonPlaceHolder jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
+        Call<NewsResponse> call = jsonPlaceHolder.getPots();
+        call.enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if(response.isSuccessful()){
+                    list = response.body().getNewsArticles();
+                    newsList = apiClient.getNewsArticals(list);
+                    Log.i("JSONNEWS", new Gson().toJson(newsList));
+                }else{
+                    Log.i("JSONNEWS", String.valueOf(response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.i("JSONNEWS", "ERROR : " +t.getMessage());
+            }
+        });
+
+        return retrofit;
     }
 
 }
--- app/src/main/java/com/chamalwr/mtopicnewsreader/functions/ApiClient.java
+++ app/src/main/java/com/chamalwr/mtopicnewsreader/functions/ApiClient.java
@@ -1,63 +1,21 @@
 package com.chamalwr.mtopicnewsreader.functions;
 
-import android.util.Log;
-
 import com.chamalwr.mtopicnewsreader.models.News;
-import com.chamalwr.mtopicnewsreader.models.NewsResponse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-
-    private static Retrofit retrofit = null;
+    
     List<News> list = new ArrayList<>();
     Map<String, String> newsList;
 
     public Map<String, String> getNewsList() {
         return newsList;
     }
-
-    public Retrofit getClient(OkHttpClient.Builder httpClient){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://newsapi.org/v2/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
-
-
-        JsonPlaceHolder jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
-        Call<NewsResponse> call = jsonPlaceHolder.getPots();
-        call.enqueue(new Callback<NewsResponse>() {
-            @Override
-            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
-                if(response.isSuccessful()){
-                    list = response.body().getNewsArticles();
-                    newsList = getNewsArticals(list);
-
-                }else{
-                    Log.i("JSONNEWS", String.valueOf(response.code()));
-                }
-            }
-
-            @Override
-            public void onFailure(Call<NewsResponse> call, Throwable t) {
-                Log.i("JSONNEWS", "ERROR : " +t.getMessage());
-            }
-        });
-
-        return retrofit;
-    }
-
 
     public Map<String, String> getNewsArticals(List<News> newsList){
 
